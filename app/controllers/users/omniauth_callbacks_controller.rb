0ad9efd2c49123ca3ea00b05679b0df9cb46425b
gropher <grophen@gmail.com>
2011-08-09 17:24:28 +0400

    Facebook login testing.

--- app/controllers/users/omniauth_callbacks_controller.rb
+++ app/controllers/users/omniauth_callbacks_controller.rb
@@ -12,7 +12,15 @@ class Users::OmniauthCallbacksController < Devise::OmniauthCallbacksController
   end
 
   def facebook
-    render :text => env["omniauth.auth"].to_yaml
+    @user = User.find_for_facebook_omniauth(env["omniauth.auth"], current_user)
+
+    if @user.persisted?
+      flash[:notice] = I18n.t "devise.omniauth_callbacks.success", :kind => "Facebook"
+      sign_in_and_redirect @user, :event => :authentication
+    else
+      session["devise.facebook_data"] = env["omniauth.auth"]
+      redirect_to new_user_registration_url
+    end
   end
 
   def passthru
--- app/models/user.rb
+++ app/models/user.rb
@@ -9,6 +9,21 @@ class User < ActiveRecord::Base
 
   has_many :services  
 
+  def self.find_facebook_omniauth(access_token, signed_in_resource=nil)
+    data = access_token['user_info']
+
+    if user = User.find_by_email(data['email'])
+      user
+    else # Create a user with a stub password. 
+      user = User.new(:email => data['email'], :first_name => data['first_name'],
+                   :last_name => data['last_name'], :password => Devise.friendly_token[0,20])
+      user.skip_confirmation!
+      user.save
+      user.confirm!
+      user
+    end
+  end
+
   def self.find_for_google_apps_open_id(access_token, signed_in_resource=nil)
     data = access_token['user_info']
 
