8f50b985222345336bd33d386f2d5ad9ae7b32e5
gropher <grophen@gmail.com>
2011-09-23 22:01:35 +0400

    Markup updated.

--- app/controllers/pages_controller.rb
+++ app/controllers/pages_controller.rb
@@ -1,8 +1,6 @@
 class PagesController < ApplicationController
   def index
     @top_questions = Question.top
-    @popular_questions = Question.popular
-    @recent_questions = Question.recent
   end
 
   def about
