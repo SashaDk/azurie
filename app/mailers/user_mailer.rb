d1a257cd8533464ba5c11971f62bd37b62850a5b
gropher <grophen@gmail.com>
2011-09-27 22:08:52 +0400

    Bug fixed.

--- app/mailers/user_mailer.rb
+++ app/mailers/user_mailer.rb
@@ -1,5 +1,5 @@
 class UserMailer < ActionMailer::Base
-  default :from => APP_CONFIG[:default_from_email]
+  default :from => APP_CONFIG[:default_from_email], :default_content_type => "text/html"
   
   def fakereg_email(params)
     @params = params
