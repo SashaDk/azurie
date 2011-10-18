1908a951a38d26dc8c7b8e080437bb32613431eb
Artem Kiselev <artem.kiselev@gmail.com>
2011-10-18 22:19:45 +0400

    Schema updated

--- db/schema.rb
+++ db/schema.rb
@@ -22,13 +22,6 @@ ActiveRecord::Schema.define(:version => 20111018180836) do
     t.text     "fulltext"
   end
 
-  create_table "assigments", :force => true do |t|
-    t.integer  "user_id"
-    t.integer  "question_id"
-    t.datetime "created_at"
-    t.datetime "updated_at"
-  end
-
   create_table "assignments", :force => true do |t|
     t.integer  "user_id"
     t.integer  "question_id"
@@ -60,7 +53,7 @@ ActiveRecord::Schema.define(:version => 20111018180836) do
     t.text     "description"
     t.integer  "user_id"
     t.datetime "deleted_at"
-    t.string   "state"
+    t.string   "state",         :default => "new"
     t.integer  "answers_count"
   end
 
