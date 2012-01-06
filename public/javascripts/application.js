d614ba01045cf7381049d44a57afc4f6885da517
Artem Kiselev <artem.kiselev@gmail.com>
2012-01-07 00:41:35 +0400

    Closes #53, closes #47: video teasers added to briefings

--- public/javascripts/application.js
+++ public/javascripts/application.js
@@ -1,2 +1,13 @@
 // Place your application-specific JavaScript functions and classes here
 // This file is automatically included by javascript_include_tag :defaults
+
+replaceVideoTeaser = function (elem, video) {
+    $(elem).hide();
+    $(elem).parent().append($(video));
+}
+
+$(function(){
+    $(".video-teaser").each(function(){
+        $(this).append("<div class='video-teaser-icon'></div>");    
+    });    
+});
