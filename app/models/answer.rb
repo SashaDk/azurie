2e2fbf7714800b9eea6dcfda115db65bac33bc76
gropher <grophen@gmail.com>
2012-01-10 01:52:20 +0400

    New comments added to answers.

--- app/models/answer.rb
+++ app/models/answer.rb
@@ -3,6 +3,7 @@ class Answer < ActiveRecord::Base
   acts_as_commentable
   belongs_to :question, :counter_cache => true
   belongs_to :user
+  has_many :comments, :as => :commentable, :dependent => :destroy, :order => :lft, :include => :user
   attr_accessible :text, :fulltext, :book_name, :book_author, :book_link
   validates :text, :presence => true
 end
