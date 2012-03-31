a3d6214736270abab83f5a7469b5c70d95145068
gropher <grophen@gmail.com>
2012-04-01 01:22:11 +0400

    Pagination bug fixed

--- app/helpers/application_helper.rb
+++ app/helpers/application_helper.rb
@@ -50,25 +50,30 @@ module ApplicationHelper
   end
 
   JS_ESCAPE_MAP = {
-        '\\' => '\\\\',
-        '</' => '<\/',
-        "\r\n" => '\n',
-        "\n" => '\n',
-        "\r" => '\n',
-        '"' => '\\"',
-        "'" => "\\'",
-        "\342\200\250" => '&#x2028;' }
+    '\\' => '\\\\',
+    '</' => '<\/',
+    "\r\n" => '\n',
+    "\n" => '\n',
+    "\r" => '\n',
+    '"' => '\\"',
+    "'" => "\\'"
+  }
 
-      # Escape carrier returns and single and double quotes for JavaScript segments.
-      # Also available through the alias j(). This is particularly helpful in JavaScript responses, like:
-      #
-      # $('some_element').replaceWith('<%=j render 'some/element_template' %>');
-      def my_escape_javascript(javascript)
-        if javascript
-          result = javascript.gsub(/(\\|<\/|\r\n|\342\200\250|[\n\r"'])/) {|match| JS_ESCAPE_MAP[match] }
-          javascript.html_safe? ? result.html_safe : result
-        else
-          ''
-        end
-      end
+  JS_ESCAPE_MAP["\342\200\250".force_encoding('UTF-8').encode!] = '&#x2028;'
+  JS_ESCAPE_MAP["\342\200\251".force_encoding('UTF-8').encode!] = '&#x2029;'
+  
+
+  # Escapes carriage returns and single and double quotes for JavaScript segments.
+  #
+  # Also available through the alias j(). This is particularly helpful in JavaScript responses, like:
+  #
+  # $('some_element').replaceWith('<%=j render 'some/element_template' %>');
+  def my_escape_javascript(javascript)
+    if javascript
+      result = javascript.gsub(/(\\|<\/|\r\n|\342\200\250|\342\200\251|[\n\r"'])/u) {|match| JS_ESCAPE_MAP[match] }
+      javascript.html_safe? ? result.html_safe : result
+    else
+      ''
+    end
+  end
 end
