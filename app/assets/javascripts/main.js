dab6a14273c8a4afa01b66260d41d4b2278766d2
gropher <grophen@gmail.com>
2012-08-06 20:14:20 +0400

    Minor bugfix

---
+++ app/assets/javascripts/main.js
@@ -0,0 +1,45 @@
+// Place your application-specific JavaScript functions and classes here
+// This file is automatically included by javascript_include_tag :defaults
+
+replaceVideoTeaser = function (elem, video) {
+    $(elem).hide();
+    $(elem).parent().append($(video));
+}
+
+$(function(){
+    $(".video-teaser").each(function(){
+        $(this).append("<div class='video-teaser-icon'></div>");    
+    });    
+});
+
+function fb_edge_create_callback(params) {
+  return social_handler("type=like&act=inc");
+}
+
+function fb_edge_remove_callback(params) {
+  return social_handler("type=like&act=dec");
+}
+
+function fb_comment_create_callback(params) {
+  return social_handler("type=comment&act=inc");
+}
+
+function fb_comment_remove_callback(params) {
+  return social_handler("type=comment&act=dec");
+}
+
+function tweet_callback(params) {
+  return social_handler("type=like&act=inc");
+}
+
+function gplusone_callback(params) {
+  var act = params.state == 'on' ? "inc" : "dec" ;
+  return social_handler("type=like&act=" + act);
+}
+
+
+function social_handler(params) {
+  //alert('You liked: ' + document.URL + "/social_handler?" + params);
+  $.get(document.URL + "/social_handler?" + params, function(data){});
+  return true;
+}
