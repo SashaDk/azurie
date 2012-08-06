5bc58ab4f78f744d4277c6a7bc272e91f41ddf4f
gropher <grophen@gmail.com>
2012-08-06 18:06:35 +0400

    After omniauth redirect added

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -29,4 +29,8 @@ class ApplicationController < ActionController::Base
     current_user
   end
 
+  def after_sign_in_path_for(resource)
+    return request.env['omniauth.origin'] || stored_location_for(resource) || root_path
+  end
+
 end
